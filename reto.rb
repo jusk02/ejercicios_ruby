5f732d1d7e1716ff930cc4849d6adc345568892f
juan david ramirez <juskhc@gmail.com>
2016-02-15 22:58:31 -0500

    reto terminado

--- reto.rb
+++ reto.rb
@@ -1,13 +1,29 @@
-suma1=0
-suma2=0
 
-for i in 1 .. 1000000
-	for x in 1 .. i
 
+for i in 1 .. 66928
+	suma1=0
+	suma2=0
+
+	for x in 1 .. (i/2)
 		if i % x == 0
+		
 		suma1+=x
 		end
-
-
-
+	end
+	for y in 1 .. (suma1/2)
+		if suma1 % y == 0
+			
+		suma2+=y
+		end
+	end
+	
+	
+	if (suma2 == i) 
+		x=x*2
+		if (suma1 != x) and (x<suma1)
+		puts "#{x} y #{suma1} son numeros amigos"
+		
+	    end
+	end
+	
 end
